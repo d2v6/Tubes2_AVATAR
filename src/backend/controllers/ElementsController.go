1257009ef1c2ca1b94377e3f2a5135d58975ae48
DanielDPW <150114531+DanielDPW@users.noreply.github.com>
2025-05-13 19:00:19 +0700

    feat: backend choice for multithreading

--- src/backend/controllers/ElementsController.go
+++ src/backend/controllers/ElementsController.go
@@ -69,7 +69,75 @@ func StartBFS(targetName string, n int, treeChan chan *TreeNode) (*TreeNode, int
 	return mergeTree(trees), NodesVisited, time.Since(start)
 }
 
-func dfs(target *elementsModel.ElementNode, limit int64, treeChan chan *TreeNode) []*TreeNode {
+func StartDFSMulti(targetName string, n int, treeChan chan *TreeNode) (*TreeNode, int64, time.Duration) {
+	atomic.StoreInt64(&NodesVisited, 0)
+	start := time.Now()
+	node, err := elementsModel.GetInstance().GetElementNode(targetName)
+	if err != nil {
+		return nil, NodesVisited, 0
+	}
+
+	trees := dfsMulti(node, int64(n), treeChan)
+	return mergeTree(trees), NodesVisited, time.Since(start)
+}
+
+func StartBFSMulti(targetName string, n int, treeChan chan *TreeNode) (*TreeNode, int64, time.Duration) {
+	atomic.StoreInt64(&NodesVisited, 0)
+	start := time.Now()
+	node, err := elementsModel.GetInstance().GetElementNode(targetName)
+	if err != nil {
+		return nil, NodesVisited, 0
+	}
+
+	trees := bfsMulti(node, int64(n), treeChan)
+	return mergeTree(trees), NodesVisited, time.Since(start)
+}
+
+func dfs(target *elementsModel.ElementNode, n int64, treeChan chan *TreeNode) []*TreeNode {
+	if target == nil {
+		return nil
+	}
+
+	if len(target.Parents) <= 0 || target.Element.Tier == 0 {
+		node := &TreeNode{
+			Name: target.Element.Name,
+		}
+		return []*TreeNode{node}
+	}
+
+	var results []*TreeNode
+
+	for _, recipe := range target.Parents {
+		leftTrees := dfs(recipe.SourceNodes[0], n, treeChan)
+		rightTrees := dfs(recipe.SourceNodes[1], n, treeChan)
+
+		for _, left := range leftTrees {
+			for _, right := range rightTrees {
+				node := &TreeNode{
+					Name:   target.Element.Name,
+					Recipe: []*TreeNode{left, right},
+				}
+
+				treeChan <- node
+
+				results = append(results, node)
+				if len(results) >= int(n) {
+					return results
+				}
+			}
+			if len(results) >= int(n) {
+				return results
+			}
+		}
+		if len(results) >= int(n) {
+			return results
+		}
+	}
+
+	return results
+}
+
+func dfsMulti(target *elementsModel.ElementNode, limit int64, treeChan chan *TreeNode) []*TreeNode {
 	if target == nil {
 		return nil
 	}
@@ -94,8 +162,8 @@ func dfs(target *elementsModel.ElementNode, limit int64, treeChan chan *TreeNode
 			}
 
 			atomic.AddInt64(&NodesVisited, 1)
-			leftTrees := dfs(recipe.SourceNodes[0], limit, treeChan)
-			rightTrees := dfs(recipe.SourceNodes[1], limit, treeChan)
+			leftTrees := dfsMulti(recipe.SourceNodes[0], limit, treeChan)
+			rightTrees := dfsMulti(recipe.SourceNodes[1], limit, treeChan)
 
 			localResults := []*TreeNode{}
 			for _, left := range leftTrees {
@@ -323,6 +391,131 @@ func bfs(target *elementsModel.ElementNode, n int64, treeChan chan *TreeNode) []
 	}
 
 	targetWg.Wait()
+	return results
+}
+
+func bfsMulti(target *elementsModel.ElementNode, n int64, treeChan chan *TreeNode) []*TreeNode {
+	if target == nil {
+		return nil
+	}
+
+	type QueueItem struct {
+		Element string
+	}
+
+	if len(target.Parents) <= 0 || target.Element.Tier == 0 {
+		node := &TreeNode{
+			Name: target.Element.Name,
+		}
+		return []*TreeNode{node}
+	}
+
+	elementToTree := make(map[string][]*TreeNode)
+	processedElements := make(map[string]bool)
+
+	currentQueue := []*QueueItem{}
+
+	for _, recipe := range target.Parents {
+		currentQueue = append(currentQueue,
+			&QueueItem{Element: recipe.Recipe.Ingredients[0]},
+			&QueueItem{Element: recipe.Recipe.Ingredients[1]},
+		)
+	}
+
+	var results []*TreeNode
+
+	for len(currentQueue) > 0 {
+		nextQueue := []*QueueItem{}
+		for len(currentQueue) > 0 {
+			current := currentQueue[0]
+			currentQueue = currentQueue[1:]
+
+			if processedElements[current.Element] {
+				continue
+			}
+
+			currentNode, err := elementsModel.GetInstance().GetElementNode(current.Element)
+			if err != nil || currentNode == nil {
+				continue
+			}
+
+			if currentNode.Element.Tier == 0 || len(currentNode.Parents) == 0 {
+				tree := &TreeNode{
+					Name: currentNode.Element.Name,
+				}
+
+				treeChan <- tree
+
+				elementToTree[currentNode.Element.Name] = []*TreeNode{tree}
+				processedElements[currentNode.Element.Name] = true
+			}
+
+			allReady := true
+			for _, recipe := range currentNode.Parents {
+				if !processedElements[recipe.Recipe.Ingredients[0]] {
+					nextQueue = append(nextQueue, &QueueItem{Element: recipe.Recipe.Ingredients[0]})
+					allReady = false
+				}
+				if !processedElements[recipe.Recipe.Ingredients[1]] {
+					nextQueue = append(nextQueue, &QueueItem{Element: recipe.Recipe.Ingredients[1]})
+					allReady = false
+				}
+			}
+
+			if !allReady {
+				nextQueue = append(nextQueue, current)
+				continue
+			}
+
+			var trees []*TreeNode
+			for _, recipe := range currentNode.Parents {
+				leftTrees := elementToTree[recipe.Recipe.Ingredients[0]]
+				rightTrees := elementToTree[recipe.Recipe.Ingredients[1]]
+				if leftTrees == nil || rightTrees == nil {
+					continue
+				}
+				for _, left := range leftTrees {
+					for _, right := range rightTrees {
+						node := &TreeNode{
+							Name:   currentNode.Element.Name,
+							Recipe: []*TreeNode{left, right},
+						}
+
+						treeChan <- node
+
+						trees = append(trees, node)
+					}
+				}
+			}
+			if len(trees) > 0 {
+				elementToTree[currentNode.Element.Name] = trees
+				processedElements[currentNode.Element.Name] = true
+			}
+		}
+		currentQueue = nextQueue
+	}
+
+	for _, recipe := range target.Parents {
+		leftTrees := elementToTree[recipe.Recipe.Ingredients[0]]
+		rightTrees := elementToTree[recipe.Recipe.Ingredients[1]]
+		if leftTrees == nil || rightTrees == nil {
+			continue
+		}
+
+		for _, left := range leftTrees {
+			for _, right := range rightTrees {
+				node := &TreeNode{
+					Name:   target.Element.Name,
+					Recipe: []*TreeNode{left, right},
+				}
+				results = append(results, node)
+				if len(results) >= int(n) {
+					return results
+				}
+			}
+		}
+	}
+
 	return results
 }
 
